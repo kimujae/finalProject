21113d31b8c8f5bbb01b906cdf8f009c9e5dea1d
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-18 22:00:21 +0900

    [ADD] UserService @service 인터페이스 메서드 정의

--- src/main/java/com/kujproject/kuj/service/UserService.java
+++ src/main/java/com/kujproject/kuj/service/UserService.java
@@ -3,5 +3,8 @@ package com.kujproject.kuj.service;
 import com.kujproject.kuj.entity.User;
 
 public interface UserService {
-
+    public void createUser();
+    public boolean login();
+    public boolean deleteUser();
+    // public boolean modifyUserInfo(User user);
 }
