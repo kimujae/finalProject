b7303ec5cf7d81a9dd7ac328192dd3a833b5deeb
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-19 22:07:43 +0900

    [FIX] User @Entity 속성 제약조건 추가 및 gradle 의존성 추가(javax.validation, spring-boot-starter-validation)

--- src/main/java/com/kujproject/kuj/entity/User.java
+++ src/main/java/com/kujproject/kuj/entity/User.java
@@ -1,10 +1,14 @@
 package com.kujproject.kuj.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @NoArgsConstructor
@@ -13,10 +17,17 @@ import lombok.Setter;
 public class User{
     @Id
     String userId;
+    @NotEmpty(message = "비밀번호는 필수 항목입니다.")
     String passWord;
+    @NotEmpty(message = "이름은 필수 항목입니다.")
     String name;
+
+    @Column(unique = true)
+    @Email(message = "유효한 이메일을 입력해주세요.")
     String email;
+    @NotEmpty(message = "전화번호는 필수 항목입니다.")
     String phone_num;
+
     String role;
 
     public User(String userId, String passWord, String name, String email, String phone_num, String role){
