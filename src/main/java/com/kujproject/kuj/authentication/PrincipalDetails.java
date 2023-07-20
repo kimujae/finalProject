057169013682f0293ccba1597da8ee803c92ed94
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-20 13:56:25 +0900

    [FIX] PrincipalDetails (UserDetails 구현체) @Override 메소드 정의

--- src/main/java/com/kujproject/kuj/authentication/PrincipalDetails.java
+++ src/main/java/com/kujproject/kuj/authentication/PrincipalDetails.java
@@ -4,48 +4,60 @@ import com.kujproject.kuj.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PrincipalDetails implements UserDetails {
 
     private User user;
-    public PrincipalDetails(User user){
+    public PrincipalDetails(User user) {
         this.user = user;
     }
+
+
+
     // 해당 유저의 권한 반환
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-
-        return null;
+        // return type이 string 이므로, Collection<GrantedAuthority>에 넣어서 반환해 줌
+        Collection<GrantedAuthority> grantedAuthoritieslist = new ArrayList<>();
+        grantedAuthoritieslist.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+        return grantedAuthoritieslist;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassWord();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getName();
     }
 
+    //===============       [추후 정의]       =================
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
