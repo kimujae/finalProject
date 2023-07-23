9f153f9ac214fdb644045772f8a79b50c6df48db
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-23 21:06:53 +0900

    [FIX] Validator 중복검증 로직 작성

--- src/main/java/com/kujproject/kuj/validation/user/CustomUserValidator.java
+++ src/main/java/com/kujproject/kuj/validation/user/CustomUserValidator.java
@@ -1,27 +1,42 @@
 package com.kujproject.kuj.validation.user;
 
+import com.kujproject.kuj.domain.service.UserService;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class CustomUserValidator {
 
     public static class UserIdValidator implements ConstraintValidator<IdDupCheckValidator, String> {
+
+        private final UserService userService;
+
+        public UserIdValidator(UserService userService) {
+            this.userService = userService;
+        }
+
         @Override
         public boolean isValid(String value, ConstraintValidatorContext context) {
-            //
-
-
-            return false;
+            if(userService.existByUserId(value)) {
+                return false;
+            }
+            return true;
         }
     }
 
     public static class UserEmailValidator implements ConstraintValidator<EmailDupCheckValidator, String> {
+        private final UserService userService;
+
+        public UserEmailValidator(UserService userService) {
+            this.userService = userService;
+        }
+
         @Override
         public boolean isValid(String value, ConstraintValidatorContext context) {
-            //
-
-
-            return false;
+            if(userService.existByEmail(value)) {
+                return false;
+            }
+            return true;
         }
     }
 }
