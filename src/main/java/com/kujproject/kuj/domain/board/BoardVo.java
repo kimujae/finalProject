1028e4e4742fb1076969538f7408b5957892231e
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-25 16:30:49 +0900

    [ADD] BoardVo 멤버필드 정의 및 @AllArgsConstructor, @Getter 선언

--- src/main/java/com/kujproject/kuj/domain/board/BoardVo.java
+++ src/main/java/com/kujproject/kuj/domain/board/BoardVo.java
@@ -1,4 +1,12 @@
 package com.kujproject.kuj.domain.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class BoardVo {
+    Long boardId;
+    String title;
+    String isPublic;
 }
