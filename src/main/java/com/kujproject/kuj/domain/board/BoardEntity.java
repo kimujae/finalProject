7e2c7599ce1feee0b6b9d2b5ced473a31f87b059
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-25 16:34:18 +0900

    [ADD] BoardEntity @Entity, attribute 선언 및 @Id설정

--- src/main/java/com/kujproject/kuj/domain/board/BoardEntity.java
+++ src/main/java/com/kujproject/kuj/domain/board/BoardEntity.java
@@ -1,4 +1,22 @@
 package com.kujproject.kuj.domain.board;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "board")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class BoardEntity {
+    @Id
+    Long boardId;
+    String title;
+    String isPublic;
+
+    // member , list, starred 와 연관관계 선언
 }
