44e3bffc08f41f801bdbb38594d85cbf026b4c35
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-07-30 15:39:56 +0900

    [ADD] ListDao @Repository 정의

---
+++ src/main/java/com/kujproject/kuj/domain/repository/ListDao.java
@@ -0,0 +1,12 @@
+package com.kujproject.kuj.domain.repository;
+
+import com.kujproject.kuj.domain.list.ListEntity;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ListDao extends Repository<ListEntity, String> {
+    Optional<List<ListEntity>> findAllByBoardId(Long boardId);
+    Optional<ListEntity> findByListId(Long listId);
+}
