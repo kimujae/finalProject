c0e7de812cade6bcb1b8e2c7625ed9892e9c9ca8
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-04 16:07:01 +0900

    [FIX] ChecklistController Read메소드 수정

--- src/main/java/com/kujproject/kuj/web/controller/ChecklistController.java
+++ src/main/java/com/kujproject/kuj/web/controller/ChecklistController.java
@@ -3,15 +3,18 @@ package com.kujproject.kuj.web.controller;
 import com.kujproject.kuj.domain.checklist.ChecklistEntity;
 import com.kujproject.kuj.domain.service.CardService;
 import com.kujproject.kuj.domain.service.ChecklistService;
+import com.kujproject.kuj.dto.checklist.ChecklistRespDto;
 import com.kujproject.kuj.dto.checklist.CreateChecklistReqDto;
 import com.kujproject.kuj.dto.checklist.UpdateProgressReqDto;
 import com.kujproject.kuj.dto.checklist.UpdateTitleReqDto;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,7 +31,7 @@ public class ChecklistController {
 
     @PostMapping("/card/{id}/checklist")
     public ResponseEntity<?> createChecklist(
-            @PathVariable Long id, CreateChecklistReqDto createChecklistReqDto, BindingResult bindingResult) {
+            @PathVariable Long id, @Valid @RequestBody CreateChecklistReqDto createChecklistReqDto, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             Map<String, String> errorMap = new HashMap<>();
 
@@ -40,22 +43,38 @@ public class ChecklistController {
         }
 
         checklistService.createChecklist(createChecklistReqDto, id);
-        return null;
+        return ResponseEntity.ok().body(createChecklistReqDto);
     }
 
-    @GetMapping("/card/{id}/checklist")
-    public ResponseEntity<?> findAllChecklist(@PathVariable Long id) {
-        List<ChecklistEntity> checklistEntityList = cardService.findAllChecklistByCardId(id);
 
-        if(checklistEntityList != null) {
-            ResponseEntity.ok().body(checklistEntityList);
+    /*
+        List<Entity> 를 반환하려하면 json 직렬화 문제 발생 -> 무한루프 -> 해결방안은 찾아야된다.
+        따라서 해결책으로 dto변환을 사용 -> 비용발생
+        어떻게 해결하는 것이 나을까?
+     */
+    @GetMapping("/card/{id}/checklist")
+    public ResponseEntity<List<ChecklistRespDto>> findAllChecklist(@PathVariable Long id) {
+        List<ChecklistRespDto> checklistRespDtoList = cardService.findAllChecklistByCardId(id);
+
+        if(checklistRespDtoList != null) {
+            return ResponseEntity.ok().body(checklistRespDtoList);
         }
         return null;
     }
 
+    @GetMapping("/checklist/{id}")
+    public ResponseEntity<ChecklistRespDto> findChecklistById(@PathVariable Long id) {
+        ChecklistRespDto checklistRespDto = checklistService.findChecklistById(id);
+
+        if(checklistRespDto != null) {
+            return ResponseEntity.ok().body(checklistRespDto);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     @PatchMapping("/checklist/{id}/progress")
     public ResponseEntity<?> updateProgress(
-            @PathVariable Long id, UpdateProgressReqDto updateProgressReqDto, BindingResult bindingResult) {
+            @PathVariable Long id, @Valid @RequestBody UpdateProgressReqDto updateProgressReqDto, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             Map<String, String> errorMap = new HashMap<>();
 
@@ -73,7 +92,7 @@ public class ChecklistController {
 
     @PatchMapping("/checklist/{id}/title")
     public ResponseEntity<?> updateTitle(
-            @PathVariable Long id, UpdateTitleReqDto updateTitleReqDto, BindingResult bindingResult) {
+            @PathVariable Long id, @Valid @RequestBody UpdateTitleReqDto updateTitleReqDto, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             Map<String, String> errorMap = new HashMap<>();
 
@@ -90,7 +109,7 @@ public class ChecklistController {
 
 
     @DeleteMapping("/checklist/{id}")
-    public ResponseEntity<HttpStatus> deleteChecklistById(@PathVariable Long id) {
+    public ResponseEntity<?> deleteChecklistById(@PathVariable Long id) {
         boolean isDeleted = checklistService.deleteChecklistById(id);
 
         if(isDeleted) {
