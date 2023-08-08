ae29a72c89fef708c0b24b1c7d15dd3f2a094c57
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-08 16:15:54 +0900

    [REFACTOR] UserService @Service 메서드 반환 타입 조정 및 예외처리

--- src/main/java/com/kujproject/kuj/domain/service/UserService.java
+++ src/main/java/com/kujproject/kuj/domain/service/UserService.java
@@ -3,28 +3,22 @@ package com.kujproject.kuj.domain.service;
 import com.kujproject.kuj.domain.board.BoardEntity;
 import com.kujproject.kuj.domain.user.UserEntity;
 import com.kujproject.kuj.dto.user.*;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public interface UserService {
-    UserEntity signUp(SignUpReqDto signUpReqDto);
-
-    Optional<UserRespDto> findUserById(String id);
-
-    List<BoardEntity>findUsersBoard(String id);
-    Optional<List<UserEntity>> findAllUser();
-
-    UpdateProfileDto updateUserProfile(String userId, UpdateProfileDto updateProfileDto);
-
-    UpdateEmailDto updateEmail(String userId, UpdateEmailDto updateEmailDto);
-
-    boolean updatePassword(String userId, UpdatePasswordDto updatePasswordDto);
-
-    boolean deleteUser(String userId);
-
+    UserRespDto signUp(SignUpReqDto signUpReqDto);
+    UserRespDto findUserById(String id) throws EntityNotFoundException;
+    List<BoardEntity>findUsersBoard(String id) throws EntityNotFoundException;
+    List<UserRespDto> findAllUser();
+    UpdateProfileDto updateUserProfile(String userId, UpdateProfileDto updateProfileDto) throws EntityNotFoundException;
+    UpdateEmailDto updateEmail(String userId, UpdateEmailDto updateEmailDto) throws EntityNotFoundException;
+    void updatePassword(String userId, UpdatePasswordDto updatePasswordDto) throws EntityNotFoundException;
+    void deleteUser(String userId) throws EntityNotFoundException;
     boolean existByUserId(String userId);
-
     boolean existByEmail(String email);
+    boolean existByPhoneNum(String phoneNum);
 }
