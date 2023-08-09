559d90b2555eefa1613374007c159ced328d124a
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-09 20:11:13 +0900

    [DELETE] BusinessExceptionHandler @Builder 생성자 삭제

--- src/main/java/com/kujproject/kuj/web/config/exception/BusinessExceptionHandler.java
+++ src/main/java/com/kujproject/kuj/web/config/exception/BusinessExceptionHandler.java
@@ -9,12 +9,6 @@ public class BusinessExceptionHandler extends RuntimeException{
     private final  ErrorCode errorCode;
 
     @Builder
-    public BusinessExceptionHandler(String message, ErrorCode errorCode) {
-        super(message);
-        this.errorCode = errorCode;
-    }
-
-    @Builder
     public BusinessExceptionHandler(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
