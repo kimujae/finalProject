210a8eed6bb36f059cbf9f0dcb3f31801547872c
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-09 23:00:37 +0900

    [REFACTOR] BoardDao @Repository 쿼리 반환타입 조정 및 BoardService @Service 비즈니스 로직 dto 반환으로 조정

--- src/main/java/com/kujproject/kuj/domain/service/BoardService.java
+++ src/main/java/com/kujproject/kuj/domain/service/BoardService.java
@@ -1,17 +1,16 @@
 package com.kujproject.kuj.domain.service;
 
-import com.kujproject.kuj.domain.board.BoardEntity;
-import com.kujproject.kuj.domain.user.UserEntity;
 import com.kujproject.kuj.dto.board.*;
+import com.kujproject.kuj.dto.user.UserRespDto;
 
 import java.util.List;
 
 
 public interface BoardService {
     BoardRespDto findBoardByBoardId(Long boardId);
-    List<UserEntity> findMemberByBoardId(Long boardId);
-    BoardEntity createNewBoard(CreateBoardReqDto createBoardReqDto);
-    boolean deleteBoard(Long boardId);
+    List<UserRespDto> findMemberByBoardId(Long boardId);
+    BoardRespDto createNewBoard(CreateBoardReqDto createBoardReqDto);
+    void deleteBoard(Long boardId);
     UpdateBoardCoverDto updateCover(Long boardId, UpdateBoardCoverDto updateBoardCoverDto);
     UpdateBoardPubRangeDto updatePubRange(Long boardId, UpdateBoardPubRangeDto updateBoardPubRangeDto);
     UpdateBoardTitleDto updateTitle(Long boardId, UpdateBoardTitleDto updateBoardTitleDto);
