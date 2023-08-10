46101c94310110e4327a94f986ffb43529fc7b68
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-10 12:40:31 +0900

    [REFACTOR] BoardEntity @Entity List<ListEntity> 연관관계 추가, ListEntity @Entity Builder패턴 적용 및 컬럼 제약조건 추가, setProperty -> changeProperty 변경

--- src/main/java/com/kujproject/kuj/domain/list/ListEntity.java
+++ src/main/java/com/kujproject/kuj/domain/list/ListEntity.java
@@ -1,26 +1,48 @@
 package com.kujproject.kuj.domain.list;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.kujproject.kuj.domain.board.BoardEntity;
+import com.kujproject.kuj.dto.list.CreateListReqDto;
+import com.kujproject.kuj.dto.list.UpdateListOrderReqDto;
+import com.kujproject.kuj.dto.list.UpdateListTitleReqDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
 @Table(name ="list")
+@Builder(builderMethodName = "builder")
 public class ListEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long listId;
     String title;
+    @Column(unique = true)
     int listOrder;
 
     @ManyToOne
     @JoinColumn(name = "board_id")
+    @JsonManagedReference
     BoardEntity board;
+
+    public void changeTitle(UpdateListTitleReqDto updateListTitleReqDto) {
+        this.title = updateListTitleReqDto.getTitle();
+    }
+
+    public void changeListOrder(UpdateListOrderReqDto updateListOrderReqDto) {
+        this.listOrder = updateListOrderReqDto.getListOrder();
+    }
+
+    public static ListEntity convertedBy(CreateListReqDto createListReqDto, BoardEntity board) {
+        return ListEntity.builder()
+                .title(createListReqDto.getTitle())
+                .listOrder(createListReqDto.getListOrder())
+                .board(board)
+                .build();
+    }
 }
