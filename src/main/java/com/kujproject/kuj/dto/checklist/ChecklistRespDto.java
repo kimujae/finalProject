3438fef2c608f43bf4bf44cbe5c74844aafd5e47
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-11 16:22:16 +0900

    [REFACTOR] Card 연관관계 매핑 된 Comment, Checklist RespDto @Builder @Getter 적용

--- src/main/java/com/kujproject/kuj/dto/checklist/ChecklistRespDto.java
+++ src/main/java/com/kujproject/kuj/dto/checklist/ChecklistRespDto.java
@@ -1,9 +1,22 @@
 package com.kujproject.kuj.dto.checklist;
 
+import com.kujproject.kuj.domain.checklist.ChecklistEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
+@AllArgsConstructor
+@Builder(builderMethodName = "builder")
 public class ChecklistRespDto {
     String title;
     int progress;
+
+    public static ChecklistRespDto convertedBy(ChecklistEntity checklist) {
+        return ChecklistRespDto.builder()
+                .title(checklist.getTitle())
+                .progress(checklist.getProgress())
+                .build();
+    }
 }
--- src/main/java/com/kujproject/kuj/dto/comment/CommentRespDto.java
+++ src/main/java/com/kujproject/kuj/dto/comment/CommentRespDto.java
@@ -1,9 +1,21 @@
 package com.kujproject.kuj.dto.comment;
 
-import lombok.Data;
+import com.kujproject.kuj.domain.comment.CommentEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
-@Data
+@Getter
+@AllArgsConstructor
+@Builder(builderMethodName = "builder")
 public class CommentRespDto {
     String content;
     String userId; //세션정보로 fix
+
+    public static CommentRespDto convertedBy(CommentEntity comment, String userId) {
+        return CommentRespDto.builder()
+                .content(comment.getContent())
+                .userId(userId)
+                .build();
+    }
 }
