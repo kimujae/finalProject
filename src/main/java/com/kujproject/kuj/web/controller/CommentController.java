06a3aa945019c524ea5d7ae8db3e067cfbe75b44
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-12 15:02:11 +0900

    [REFACTOR] CommentController, TodoCheckController @RestController ResponseEntity<ApiResponse> 로 반환타입 조정 및 코드 단순화

--- src/main/java/com/kujproject/kuj/web/controller/CommentController.java
+++ src/main/java/com/kujproject/kuj/web/controller/CommentController.java
@@ -5,6 +5,8 @@ import com.kujproject.kuj.domain.service.CommentService;
 import com.kujproject.kuj.dto.comment.CommentRespDto;
 import com.kujproject.kuj.dto.comment.CreateCommentReqDto;
 import com.kujproject.kuj.dto.comment.UpdateContentReqDto;
+import com.kujproject.kuj.web.common.code.SuccessCode;
+import com.kujproject.kuj.web.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,57 +27,55 @@ public class CommentController {
 
 
     @PostMapping("/card/{id}/comment")
-    public ResponseEntity<?> createComment(
+    public ResponseEntity<ApiResponse> createComment(
             @PathVariable Long id, @Valid @RequestBody CreateCommentReqDto createCommentReqDto) {
 
-        createCommentReqDto = commentService.createComment(createCommentReqDto, id);
-
-        if (createCommentReqDto != null) {
-            return ResponseEntity.ok().body(createCommentReqDto);
-        }
-
-        return ResponseEntity.notFound().build();
+        CommentRespDto commentRespDto = commentService.createComment(createCommentReqDto, id);
+        return new ResponseEntity<>(ApiResponse.builder()
+                .result(commentRespDto)
+                .successCode(SuccessCode.INSERT_SUCCESS)
+                .build(), HttpStatus.CREATED);
     }
 
     @GetMapping("/card/{id}/comment")
-    public ResponseEntity<List<CommentRespDto>> findAllCommentByCardId(@PathVariable Long id) {
-        List<CommentRespDto> commentRespDtoList = cardService.findAllCommentByCardId(id);
+    public ResponseEntity<ApiResponse> findAllCommentByCardId(@PathVariable Long id) {
 
-        if(commentRespDtoList != null) {
-            return ResponseEntity.ok().body(commentRespDtoList);
-        }
-        return ResponseEntity.notFound().build();
+        List<CommentRespDto> commentRespDtoList = cardService.findAllCommentByCardId(id);
+        return new ResponseEntity<>(ApiResponse.builder()
+                .result(commentRespDtoList)
+                .successCode(SuccessCode.SELECT_SUCCESS)
+                .build(), HttpStatus.OK);
     }
 
     @GetMapping("/comment/{id}")
-    public ResponseEntity<CommentRespDto> findCommentByCommentId(@PathVariable Long id) {
-        CommentRespDto commentRespDto = commentService.findCommentById(id);
+    public ResponseEntity<ApiResponse> findCommentByCommentId(@PathVariable Long id) {
 
-        if(commentRespDto != null) {
-            return ResponseEntity.ok().body(commentRespDto);
-        }
-        return ResponseEntity.notFound().build();
+        CommentRespDto commentRespDto = commentService.findCommentById(id);
+        return new ResponseEntity<>(ApiResponse.builder()
+                .result(commentRespDto)
+                .successCode(SuccessCode.SELECT_SUCCESS)
+                .build(), HttpStatus.OK);
     }
 
     @PatchMapping("comment/{id}/content")
     public ResponseEntity<?> updateContent(
             @PathVariable Long id, @Valid @RequestBody UpdateContentReqDto updateContentReqDto) {
-        updateContentReqDto = commentService.updateContent(updateContentReqDto, id);
 
-        if(updateContentReqDto != null) {
-            return ResponseEntity.ok().body(updateContentReqDto);
-        }
-        return ResponseEntity.notFound().build();
+        updateContentReqDto = commentService.updateContent(updateContentReqDto, id);
+        return new ResponseEntity<>(ApiResponse.builder()
+                .result(updateContentReqDto)
+                .successCode(SuccessCode.INSERT_SUCCESS)
+                .build(), HttpStatus.OK);
     }
 
 
     @DeleteMapping("/comment/{id}")
-    public ResponseEntity<HttpStatus> deleteComment(@PathVariable Long id) {
-        boolean isDeleted = commentService.deleteComment(id);
+    public ResponseEntity<ApiResponse> deleteComment(@PathVariable Long id) {
 
-        if(isDeleted) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+        commentService.deleteComment(id);
+        return new ResponseEntity<>(ApiResponse.builder()
+                .result(null)
+                .successCode(SuccessCode.DELETE_SUCCESS)
+                .build(), HttpStatus.NO_CONTENT);
     }
 }
