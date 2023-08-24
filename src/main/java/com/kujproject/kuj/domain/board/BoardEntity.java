035f89a3261f0664e69e107a17184d863a5d452f
김우재(U Jae  Kim) <dnwo0103@naver.com>
2023-08-24 13:43:35 +0900

    [FIX] BoardEntity Board_UserEntity 속성  HashSet으로 변경

--- src/main/java/com/kujproject/kuj/domain/board/BoardEntity.java
+++ src/main/java/com/kujproject/kuj/domain/board/BoardEntity.java
@@ -1,6 +1,5 @@
 package com.kujproject.kuj.domain.board;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.kujproject.kuj.domain.board_user.Board_UserEntity;
 import com.kujproject.kuj.domain.list.ListEntity;
 import com.kujproject.kuj.dto.board.CreateBoardReqDto;
@@ -11,7 +10,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity(name = "board")
 @NoArgsConstructor
@@ -27,13 +28,13 @@ public class BoardEntity{
     private boolean isPublic;
 
 //     member , list, starred 와 연관관계 선언
-    @OneToMany(mappedBy = "board" , fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-    @JsonManagedReference
-    List<Board_UserEntity> users = new ArrayList<>();
+    @OneToMany(mappedBy = "board" , cascade = CascadeType.REMOVE)
+    //@JsonManagedReference
+    Set<Board_UserEntity> users = new HashSet<>();
 
 
-    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-    @JsonManagedReference
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    //@JsonManagedReference
     List<ListEntity> lists = new ArrayList<>();
 
     public void changeTitle(UpdateBoardTitleDto updateBoardTitleDto) {
